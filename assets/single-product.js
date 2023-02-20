590866519ee0812f60ba741aab591b2a9101fe57
unknown <dangvietdame@gmail.com>
2023-02-20 17:30:26 +0700

    20.2.2023

--- assets/single-product.js
+++ assets/single-product.js
@@ -1,9 +1,14 @@
 jQuery(document).ready(function($) {
-    $('.product-description-wrapper .seemore').click(function(){
+    //See more Description toggle
+    $('.single-product-layout-1 .product-description-wrapper .seemore').click(function(){
         $(this).siblings('.sur-description').toggleClass('active');
         $(this).siblings('.bread-description').toggleClass('de-active');
         $(this).children('.icon-dropdown').toggleClass('de-active');
         $(this).children('.icon-dropdown-up').toggleClass('active');
+    });
 
+    //Accordion toggle
+    $('.single-product-layout-1 .accordion-header').click(function(){
+        $(this).siblings('.accordion-content').slideToggle('300');
     });
 })
