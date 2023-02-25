7861366770ad4b5d955de25998db4ead85836b99
unknown <dangvietdame@gmail.com>
2023-02-25 11:29:40 +0700

    25.2.2023

---
+++ assets/other-single-product.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    //Change data in input quantity product
+    $('.quantity .quantity-minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.quantity .quantity-plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
