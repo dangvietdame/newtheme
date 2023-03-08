d4424bbb35d0247b8a423c7fa68a9a3de6701b98
unknown <dangvietdame@gmail.com>
2023-03-08 17:30:55 +0700

    8.3.2023

--- assets/other-single-product.js
+++ assets/other-single-product.js
@@ -15,33 +15,29 @@ $(document).ready(function() {
         return false;
     });
 });
-var selectCallback = function(variant, selector) {
+var format_money = document.querySelector('.price-container').dataset.money_format;
+function selectCallback(variant, selector) {
     // callback content
-    document.getElementById('single-product-price').innerHTML = Shopify.formatMoney(variant.price);
+    document.getElementById('single-product-price').textContent = Shopify.formatMoney(variant.price, format_money);
 
     var compare_price = document.getElementById('single-product-compare-price');
-    if (variant.compare_at_price > 0){
-        compare_price.innerHTML = Shopify.formatMoney(variant.compare_at_price);
-    }
-    else{
-        compare_price.innerHTML = '';
-    }
-    
+    compare_price.textContent = variant.compare_at_price > 0 ? Shopify.formatMoney(variant.compare_at_price, format_money) : '';
+
     var available = document.getElementById('single-product-available');
     var add_to_cart = document.getElementById('add-to-cart-button');
     var buy_now = document.getElementsByClassName('shopify-payment-button__button');
-    if (variant.available == false){
-        available.innerHTML = available.dataset.available;
-        add_to_cart.innerHTML = add_to_cart.dataset.sold_out;
-        add_to_cart.removeAttribute('class','add-to-cart-button');
-        add_to_cart.setAttribute('class','sold-out-button');
-        add_to_cart.setAttribute('disabled','disable');
+    if (variant.available == false) {
+        available.textContent = available.dataset.available;
+        add_to_cart.textContent = add_to_cart.dataset.sold_out;
+        add_to_cart.removeAttribute('class', 'add-to-cart-button');
+        add_to_cart.setAttribute('class', 'sold-out-button');
+        add_to_cart.setAttribute('disabled', 'disable');
     }
-    else{
-        available.innerHTML = '';
-        add_to_cart.innerHTML = add_to_cart.dataset.value;
-        add_to_cart.removeAttribute('class','sold-out-button');
+    else {
+        available.textContent = '';
+        add_to_cart.textContent = add_to_cart.dataset.value;
+        add_to_cart.removeAttribute('class', 'sold-out-button');
         add_to_cart.removeAttribute('disabled');
-        add_to_cart.setAttribute('class','add-to-cart-button');
+        add_to_cart.setAttribute('class', 'add-to-cart-button');
     }
 }
